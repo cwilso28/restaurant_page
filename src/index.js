7463ae1bc51e974121276df1de03891bca4858f7
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-12 09:37:28 -0500

    Add about module import and setting it as default
    Setting the about module as default helps with page development. This
    will be revised in the future after the page has been developed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./styles.css";
 import { loadHome } from "./home.js";
 import { loadMenu } from "./menu.js";
+import { loadAbout } from "./about.js";
 
-loadMenu();
+loadAbout();
+// loadMenu();
 // loadHome();
 
