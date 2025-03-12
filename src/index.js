f1a48d00289173089d76f39ac520029f95217ab0
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-12 10:36:09 -0500

    Add eventlisteners to the buttons
    The eventlisteners allow for content switching while maintaining the
    site title and buttons.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import "./styles.css";
-import { loadHome } from "./home.js";
-import { loadMenu } from "./menu.js";
-import { loadAbout } from "./about.js";
+import { homeContent } from "./home.js";
+import { menuContent } from "./menu.js";
+import { aboutContent } from "./about.js";
 
-loadAbout();
+function loadHome () {
+    homeContent();
+}
+
+function loadMenu () {
+    menuContent();
+}
+
+function loadAbout () {
+    aboutContent();
+}
+
+function clearContent () {
+    contentContainer.textContent = "";
+}
+
+let homeButtonContainer = document.getElementById("home-button");
+let menuButtonContainer = document.getElementById("menu-button");
+let aboutButtonContainer = document.getElementById("about-button");
+let contentContainer = document.getElementById("content");
+
+homeButtonContainer.addEventListener("click", function(e) {
+    clearContent();
+    loadHome();
+})
+
+menuButtonContainer.addEventListener("click", function(e) {
+    clearContent();
+    loadMenu();
+})
+
+aboutButtonContainer.addEventListener("click", function(e) {
+    clearContent();
+    loadAbout();
+})
+
+
+// aboutContent();
 // loadMenu();
-// loadHome();
+loadHome();
 
