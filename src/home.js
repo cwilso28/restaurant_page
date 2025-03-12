9821a37ff63c710b5322de9d5f03f1e2d31297cf
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-11 20:42:34 -0500

    Add id to image and reorganize element positions
    Adding an id to th image will make it easier to reference the image to
    resize it. Changing the header to be above the buttons let us use it as
    a page title. Moving the image to below the welcome text helps with the
    flow of the home page.

--- src/home.js
+++ src/home.js
@@ -4,27 +4,29 @@ function loadHome() {
 
     let contentDiv = document.getElementById("content");
     // alert("Home Loaded!")
-    let header = document.createElement("h1");
-    header.textContent = "Guy's Pies";
-    contentDiv.append(header);
+    
+
+    let header = document.querySelector("header")
+    let title = document.createElement("h1");
+    title.textContent = "Guy's Pies";
+    header.prepend(title);
+
+    let welcomeText = document.createElement("p");
+    welcomeText.textContent = "Welcome to Guy's Pies! These will be the best pies you've ever had, or your money back. We have pies for every occasion or if you're just in the mood for a sweet treat. Stop on in or order online. We deliver!";
+    contentDiv.append(welcomeText);
 
     let imageDiv = document.createElement("div");
     contentDiv.append(imageDiv);
-
+    
     let image = document.createElement("img");
     image.src = pieImage;
     image.alt = "Apple pie with a slice taken out";
+    image.id = "home-img";
     imageDiv.append(image);
 
     let imageAttr = document.createElement("p");
     imageAttr.innerHTML = 'Photo by <a href="https://unsplash.com/@hugoaitken?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Hugo Aitken</a> on <a href="https://unsplash.com/photos/apple-pie-on-black-palte-PqYvDBwpXpU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
     imageDiv.append(imageAttr);
-
-    let welcomeText = document.createElement("p");
-    welcomeText.textContent = "Welcome to Guy's Pies! These will be the best pies you've ever had, or your money back. We have pies for every occasion or if you're just in the mood for a sweet treat. Stop on in or order online. We deliver!";
-    contentDiv.append(welcomeText);
-    
-
 }
 
 export { loadHome }
