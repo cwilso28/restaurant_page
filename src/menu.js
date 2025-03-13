d0be655b5331ef6978962c61fce0fee5b623ce3f
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-12 20:49:49 -0500

    Add ids each menu item and content to the first
    The content added to the first menu item is to facilitate layout
    formatting.

--- src/menu.js
+++ src/menu.js
@@ -2,17 +2,26 @@ function menuContent() {
     let divContainer = document.getElementById("content");
 
     let itemOneContainer = document.createElement("div");
+    itemOneContainer.id = "menu-container";
     divContainer.append(itemOneContainer);
 
     let itemTwoContainer = document.createElement("div");
+    itemTwoContainer.id = "menu-container";
     divContainer.append(itemTwoContainer);
 
     let itemThreeContainer = document.createElement("div");
+    itemThreeContainer.id = "menu-container";
     divContainer.append(itemThreeContainer);
 
     let itemOneDisc = document.createElement("p");
-    itemOneDisc.textContent = "Item one placeholder";
+    let itemOneHeader = document.createElement("h2");
+    let itemOneImg = document.createElement("img");
+    itemOneHeader.textContent = "Apple Pie"
+    itemOneContainer.append(itemOneHeader);
+    itemOneDisc.textContent = "This is our apple pie. It's a modern take on a classic recipe! You won't be disappointed. WARNING! CONTAINS APPLES.";
     itemOneContainer.append(itemOneDisc);
+    itemOneImg.src = "www.example.com";
+    itemOneContainer.append(itemOneImg);
 
     let itemTwoDisc = document.createElement("p");
     itemTwoDisc.textContent = "Item two placeholder";
