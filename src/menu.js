610b831e5a014d93f423d272c4002af5f108c7c5
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-14 20:34:37 -0500

    Remove commented code

--- src/menu.js
+++ src/menu.js
@@ -65,37 +65,6 @@ function menuContent() {
 
     let pecanPieContainer = menuItem(pecanPieTitle, pecanPieDesc, pecanPiePrice, pecanPieImage, pecanPieImageDesc, pecanPieImageAttr).itemContainer;
     divContainer.append(pecanPieContainer);
-
-    // let itemOneContainer = document.createElement("div");
-    // itemOneContainer.id = "menu-container";
-    // divContainer.append(itemOneContainer);
-
-    // let itemTwoContainer = document.createElement("div");
-    // itemTwoContainer.id = "menu-container";
-    // divContainer.append(itemTwoContainer);
-
-    // let itemThreeContainer = document.createElement("div");
-    // itemThreeContainer.id = "menu-container";
-    // divContainer.append(itemThreeContainer);
-
-    // let itemOneDisc = document.createElement("p");
-    // let itemOneHeader = document.createElement("h2");
-    // let itemOneImg = document.createElement("img");
-    // itemOneHeader.textContent = "Apple Pie"
-    // itemOneContainer.append(itemOneHeader);
-    // itemOneDisc.textContent = "This is our apple pie. It's a modern take on a classic recipe! You won't be disappointed. WARNING! CONTAINS APPLES.";
-    // itemOneContainer.append(itemOneDisc);
-    // itemOneImg.src = "www.example.com";
-    // itemOneImg.alt = "Apple pie";
-    // itemOneContainer.append(itemOneImg);
-
-    // let itemTwoDisc = document.createElement("p");
-    // itemTwoDisc.textContent = "Item two placeholder";
-    // itemTwoContainer.append(itemTwoDisc);
-
-    // let itemThreeDisc = document.createElement("p");
-    // itemThreeDisc.textContent = "Item three placeholder";
-    // itemThreeContainer.append(itemThreeDisc);
 }
 
 export { menuContent }
